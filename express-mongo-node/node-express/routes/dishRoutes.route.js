eed0352af282256b945c071f62deb793e25191a8
kevinjimenez <kivito2009@hotmail.com>
2020-09-10 12:38:31 -0500

    file rutas con params

--- express-mongo-node/node-express/routes/dishRoutes.route.js
+++ express-mongo-node/node-express/routes/dishRoutes.route.js
@@ -2,9 +2,8 @@ const bodyParse = require('body-parser');
 const express = require('express');
 
 const dishRouter = express.Router();
-
 dishRouter.use(bodyParse.json());
-
+//  without params
 dishRouter.route('/')
 
 .all((req,res,next)=>{
@@ -15,11 +14,12 @@ dishRouter.route('/')
 })
 
 .get((req,res,next)=>{
-    console.log(res.headers)
+    console.log(res.headers)        
     res.end('Te enviaremos todos los platos!');
 })
 
-.post((req,res,next)=>{    
+.post((req,res,next)=>{
+    
     res.end('Añadaremos el plato: ' + req.body.name + 
     req.body.descripcion);
 })
@@ -32,5 +32,35 @@ dishRouter.route('/')
 .delete((req,res,next)=>{    
     res.end('Borrando todos los platos!');
 });
+
+//  with params
+dishRouter.route('/:dishId')
+.all((req,res,next)=>{
+    res.statusCode = 200;
+    console.log(res.headers)
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+.get((req,res,next)=>{
+    console.log(res.headers)    
+    res.end('Te enviaremos todos los platos!' + req.params.dishId);
+    //res.end('Te enviaremos todos los platos!');
+})
+
+.post((req,res,next)=>{
+    
+    res.end('Añadaremos el plato: ' + req.body.name + 
+    req.body.descripcion);
+})
+
+.put((req,res,next)=>{    
+    res.statusCode = 403;
+    res.end('PUT no es una operacion soportada en /dishes');
+})
+
+.delete((req,res,next)=>{    
+    res.end('Borrando todos los platos!');
+});
+
 
 module.exports = dishRouter;
